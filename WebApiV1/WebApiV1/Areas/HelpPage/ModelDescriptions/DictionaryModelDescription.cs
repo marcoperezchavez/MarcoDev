eb21568d8c1087aa7e59cc29839b191558c8ec5b
Marco Antonio De Jesus Perez Chavez <marcode-c@herbalife.com>
2018-05-03 14:44:04 -0500

    Added Web api project

---
+++ WebApiV1/WebApiV1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiV1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApiV1/WebApiV1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiV1.WebApiApplication" Language="C#" %>
